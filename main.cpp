6ae306eaa0753a8f68d0bdfa84e0c1835cbf803c
Charlie.com <csru22005@g.nihon-u.ac.jp>
2022-12-14 14:54:34 +0000

    v2

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include "mbed.h"
+#include "HEPTA_COM.h"
 
-DigitalOut myled(LED1);
+RawSerial pc(USBTX,USBRX,9600);
+HEPTA_COM com(p9,p10,9600);
 
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+int main()
+{
+    pc.printf("Xbee Count Up Mode\r\n");
+    for(int i = 0; i < 10; i++) {
+        com.printf("num = %d\r\n",i);
+        wait(1.0);
+    }
 }
